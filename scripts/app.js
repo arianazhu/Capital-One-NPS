7db375922fa38ddbd5994e5aee0be7dd4a5b8bd5
Ariana Zhu <zhu811@purdue.edu>
2019-05-25 16:04:41 +0800

    node js and textfile added

---
+++ scripts/app.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+
+// // var stuff = require('./stuff');
+// //
+// // console.log(stuff.counter([1, 2, 3]));
+// // console.log(stuff.adder(1,stuff.pi));
+//
+// var events = require('events');
+//
+// // EMITS
+// // var myEmitter = new events.EventEmitter();
+// // myEmitter.on('someEvent', function(msg){
+// //   console.log(msg);
+// // });
+// //
+// // myEmitter.emit('someEvent', 'the event was emitted');
+//
+// // REQUIRE
+// var util = require('util');
+//
+// var Person = function(name) {
+//   this.name = name;
+// };
+//
+// util.inherits(Person, events.EventEmitter);
+//
+// var james = new Person('james');
+// var mary = new Person('mary');
+//
+// var people = [james, mary];
+//
+// people.forEach(function(person) {
+//   person.on('speak', function(msg) {
+//     console.log(person.name + ' said: ' + msg);
+//   });
+// });
+//
+// james.emit('speak', "hey i'm james");
+// mary.emit('speak', "hey i'm mary");
---
+++ scripts/stuff.js
@@ -0,0 +1,20 @@
+// var counter = function(arr) {
+//   return 'Num elements is ' + arr.length;
+// };
+//
+// var adder = function(a, b) {
+//   return `Sum is ${a+b}`;
+// };
+//
+// var pi = 3.142;
+//
+// // module.exports.counter = counter;
+// // module.exports.adder = adder;
+// // module.exports.pi = pi;
+//
+// // or
+// module.exports = {
+//   counter: counter,
+//   adder: adder,
+//   pi: pi
+// };
