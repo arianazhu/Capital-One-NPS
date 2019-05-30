1db73b18a1f0d01c86a0b9fb456367ca2a504b5a
Ariana Zhu <zhu811@purdue.edu>
2019-05-31 00:04:18 +0800

    started work on alerts

--- controllers/parkController.js
+++ controllers/parkController.js
@@ -3,7 +3,8 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false});
 var parks = require('../assets/parks.json').data;
 var campgrounds = require('../assets/campgrounds.json').data;
 var visitors = require('../assets/visitor.json').data;
-
+var alerts = require('../assets/alerts.json').data;
+var articles = require('../assets/articles.json').data;
 
 module.exports = function(app){ // app is from app.js (var app = express();)
   app.get('/parks/:park', function(req, res){
@@ -11,7 +12,9 @@ module.exports = function(app){ // app is from app.js (var app = express();)
       parkInfo: parks,
       code: req.params.park,
       campInfo: campgrounds,
-      visitorInfo: visitors
+      visitorInfo: visitors,
+      alertInfo: alerts,
+      articleInfo: articles
     });
   });
 };
