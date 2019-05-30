e2cb497c83765349c42e8991e7a0395457f05eff
Ariana Zhu <zhu811@purdue.edu>
2019-05-30 23:13:28 +0800

    finished visitors, campgrounds

--- controllers/parkController.js
+++ controllers/parkController.js
@@ -1,9 +1,17 @@
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false});
 var parks = require('../assets/parks.json').data;
+var campgrounds = require('../assets/campgrounds.json').data;
+var visitors = require('../assets/visitor.json').data;
+
 
 module.exports = function(app){ // app is from app.js (var app = express();)
   app.get('/parks/:park', function(req, res){
-    res.render('park', { parkInfo: parks, code: req.params.park });
+    res.render('park', {
+      parkInfo: parks,
+      code: req.params.park,
+      campInfo: campgrounds,
+      visitorInfo: visitors
+    });
   });
 };
