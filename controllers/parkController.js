34a3dfd8d66819728454e983e673d02826abf68a
Ariana Zhu <zhu811@purdue.edu>
2019-05-31 13:46:15 +0800

    finalized alerts (for now)

--- controllers/parkController.js
+++ controllers/parkController.js
@@ -5,6 +5,12 @@ var campgrounds = require('../assets/campgrounds.json').data;
 var visitors = require('../assets/visitor.json').data;
 var alerts = require('../assets/alerts.json').data;
 var articles = require('../assets/articles.json').data;
+var events = require('../assets/events.json').data;
+var news = require('../assets/news.json').data;
+var lessons = require('../assets/lessons.json').data;
+var people = require('../assets/people.json').data;
+var places = require('../assets/places.json').data;
+
 
 module.exports = function(app){ // app is from app.js (var app = express();)
   app.get('/parks/:park', function(req, res){
@@ -14,7 +20,12 @@ module.exports = function(app){ // app is from app.js (var app = express();)
       campInfo: campgrounds,
       visitorInfo: visitors,
       alertInfo: alerts,
-      articleInfo: articles
+      articleInfo: articles,
+      eventInfo: events,
+      newsInfo: news,
+      lessonInfo: lessons,
+      peopleInfo: people,
+      placeInfo: places
     });
   });
 };
